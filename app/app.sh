f508a0ed0e2843ce391b5b4f162be098e6416b51
AvelFir <guichasemartinsgmr@hotmail.com>
2024-03-10 03:36:44 -0300

    improve performance

--- app/app.sh
+++ app/app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-java -Xms"${JVM_XMS}" -Xmx"${JVM_XMX}" -Duser.timezone=America/Sao_Paulo -D64 -XX:+UseG1GC \
--XX:MaxRAM="${CONTAINER_MEMORY}" -XX:NewSize="${NEW_SIZE}" -XX:MaxNewSize="${MAX_NEW_SIZE}" \
--XX:MinHeapFreeRatio="${MIN_HEAP_FREE_RATIO}" -XX:MaxHeapFreeRatio="${MAX_HEAP_FREE_RATIO}" \
--XX:+UnlockExperimentalVMOptions -XX:+UseNUMA -XX:+UseStringDeduplication -jar app.jar --server.port="${PORT}"
+java -Xms${JVM_XMS} -Xmx${JVM_XMX} -Duser.timezone=America/Sao_Paulo -Dspring.profiles.active=${PROFILE} -D64 -XX:+UseG1GC \
+-XX:MaxRAM=${CONTAINER_MEMORY} -XX:NewSize=${NEW_SIZE} -XX:MaxNewSize=${MAX_NEW_SIZE} \
+-XX:MinHeapFreeRatio=${MIN_HEAP_FREE_RATIO} -XX:MaxHeapFreeRatio=${MAX_HEAP_FREE_RATIO} \
+-XX:GCTimeRatio=1 -XX:AdaptiveSizePolicyWeight=90 -noverify -XX:TieredStopAtLevel=1 \
+-XX:+UnlockExperimentalVMOptions -XX:+UseNUMA -XX:+UseStringDeduplication -jar app.jar --server.port=${PORT}
